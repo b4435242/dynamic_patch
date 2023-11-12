7b6f182cfcc27b86bb6f09d2b21626122d069786
b4435242 <b4435242@gmail.com>
2023-11-12 20:03:07 +0800

    1112

--- dependencies/install.bat
+++ dependencies/install.bat
@@ -7,15 +7,15 @@ pip install --no-index --find-links setuptools setuptools
 
 cd cppheaderparser\CppHeaderParser-2.7.4 
 python setup.py install
-cd ..
+cd ..\..
 
 cd mulpyplexer\mulpyplexer-0.09
 python setup.py install
-cd ..
+cd ..\..
 
 cd future\future-0.18.3
 python setup.py install
-cd ..
+cd ..\..
 )
 
 pip install --no-index --find-links angr angr
